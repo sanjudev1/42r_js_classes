c8cea75e9e7328d11bca6c6155fa4eaebfe250d5
sanjeev ch <112742845+sanjudev1@users.noreply.github.com>
2025-08-06 07:35:27 +0530

    day_1 class toggle bulb task

---
+++ toggle_bulb_task/script.js
@@ -0,0 +1,14 @@
+
+ document.getElementById("title").textContent="Hello Developers"
+
+document.getElementById("title").style.color="blue";
+
+
+
+function Switchon(){
+  document.getElementById("bulb").src="https://thumbs.dreamstime.com/b/bulb-idea-9952322.jpg?w=300"
+}
+function Switchoff(){
+  document.getElementById("bulb").src="https://thumbs.dreamstime.com/b/idea-bulb-turn-off-7354247.jpg?w=300"
+}
+
